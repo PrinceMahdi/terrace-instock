0c8f40ad32659917a8fea6a00c686c080ce02ad6
Mahdi Shirmohammadi <shirmohammadi2000@gmail.com>
2022-11-21 16:20:10 -0800

    S22BCT-7 commiting changes to global files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 /* ------------------ SCSS IMPORTS ------------------ */
 import "./App.scss";
+/* ---------------- COMPONENT IMPORTS ---------------- */
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
+/* ---------------- REACT IMPORTS ---------------- */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={HomePage} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
