33f1697a0025370ed83a0895a39d2ab2e51c3fe0
Gurpreet Randhawa <singhpreet01991@gmail.com>
2022-11-23 08:47:34 -0800

    add delete inventory modal

---
+++ src/Components/Modals/InventoryDeleteModal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Modals.scss";
+import close from "../../assets/icons/close-24px.svg";
+
+const InventoryDeleteModal = () => {
+  return (
+    <>
+      <div className="modal">
+        <div className="modal__delete">
+          <h1 className="modal__title">Delete this inventory item?</h1>
+          <p className="modal__para">
+            Please confirm that you'd like to delete this from the inventory
+            list. You won't be able to undo this action.
+          </p>
+          <button className="modal__cancel">Cancel</button>
+          <button className="modal__delete-button">Delete</button>
+          <a href="/">
+            <img className="modal__icon" src={close} alt="x button" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default InventoryDeleteModal;
