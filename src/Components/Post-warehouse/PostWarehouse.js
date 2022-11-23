ca98782470baa4a74546f16c411a77e825ff129b
travis <trvsmartin@gmail.com>
2022-11-22 20:42:29 -0800

    setup PostWarehouse component
    <body>
    
    Issue #

---
+++ src/Components/Post-warehouse/PostWarehouse.js
@@ -0,0 +1,230 @@
+/* ---------------- SCSS IMPORTS ---------------- */
+import "./PostWarehouse.scss";
+
+/* ---------------- REACT IMPORTS ---------------- */
+import { useState } from "react";
+import axios from "axios";
+
+const PostWarehouse = () => {
+  // track state for form fields
+  const [nameState, setNameState] = useState("");
+  const [addressState, setAddressState] = useState("");
+  const [cityState, setCityState] = useState("");
+  const [countryState, setCountryState] = useState("");
+  const [contactState, setContactState] = useState("");
+  const [positionState, setPositionState] = useState("");
+  const [phoneState, setPhoneState] = useState("");
+  const [emailState, setEmailState] = useState("");
+
+  //track and set form content via state
+  const handleChangeName = (event) => {
+    setNameState(event.target.value);
+  };
+  const handleChangeAddress = (event) => {
+    setAddressState(event.target.value);
+  };
+  const handleChangeCity = (event) => {
+    setCityState(event.target.value);
+  };
+  const handleChangeCountry = (event) => {
+    setCountryState(event.target.value);
+  };
+  const handleChangeContact = (event) => {
+    setContactState(event.target.value);
+  };
+  const handleChangePosition = (event) => {
+    setPositionState(event.target.value);
+  };
+  const handleChangePhone = (event) => {
+    setPhoneState(event.target.value);
+  };
+  const handleChangeEmail = (event) => {
+    setEmailState(event.target.value);
+  };
+
+  const phoneRegex = new RegExp(
+    `^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$`
+  );
+  const emailRegex = new RegExp(".+@.+..+");
+
+  // check if phone valid
+  const isPhoneValid = () => {
+    if (!phoneState.match(phoneRegex)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const isEmailValid = () => {
+    if (emailState.match(emailRegex)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // check if full form valid
+  const isFormValid = () => {
+    // check each field validated for some content
+    if (
+      nameState.length < 1 ||
+      addressState.length < 1 ||
+      cityState.length < 1 ||
+      countryState < 1 ||
+      contactState < 1 ||
+      positionState < 1
+    ) {
+      return false;
+    } else return true;
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isFormValid() && isPhoneValid() && isEmailValid()) {
+      // take form field state, build newWarehouse object
+      const newWarehouse = {
+        warehouse_name: nameState,
+        address: addressState,
+        city: cityState,
+        country: countryState,
+        contact_name: contactState,
+        contact_position: positionState,
+        contact_phone: phoneState,
+        contact_email: emailState,
+      };
+      console.log(newWarehouse);
+      // send form to API
+      // TODO: this is post rather than put patch
+      axios
+        .post(`http://localhost:8080/warehouses`, newWarehouse)
+        .then((response) => {
+          console.log(response.data);
+          event.target.reset();
+        });
+      alert("success");
+    } else {
+      console.log("form error");
+    }
+  };
+  return (
+    <div className="warehouse__wrapper">
+      <div className="warehouse__header">
+        <a>
+          <div className="warehouse__back"></div>
+        </a>
+        <h2 className="warehouse__edit">Add New Warehouse</h2>
+      </div>
+      <form onSubmit={handleSubmit} className="warehouse__form">
+        <div className="warehouse__form-container">
+          <div className="warehouse__form-top">
+            <h3 className="warehouse__form-subhead">Warehouse Details</h3>
+            <label className="warehouse__form-label" htmlFor="warehouseName">
+              Warehouse Name
+            </label>
+            <input
+              type="text"
+              placeholder="Warehouse Name"
+              className="warehouse__input"
+              name="warehouseName"
+              onChange={handleChangeName}
+              value={nameState}
+            />
+            <label className="warehouse__form-label" htmlFor="warehouseAddress">
+              Warehouse Address
+            </label>
+            <input
+              type="text"
+              placeholder="Address"
+              className="warehouse__input"
+              name="warehouseAddress"
+              onChange={handleChangeAddress}
+              value={addressState}
+            />
+            <label className="warehouse__form-label" htmlFor="warehouseCity">
+              City
+            </label>
+            <input
+              type="text"
+              placeholder="City"
+              className="warehouse__input"
+              name="warehouseCity"
+              onChange={handleChangeCity}
+              value={cityState}
+            />
+            <label className="warehouse__form-label" htmlFor="">
+              Country
+            </label>
+            <input
+              type="text"
+              placeholder="Country"
+              className="warehouse__input"
+              name="warehouseCountry"
+              onChange={handleChangeCountry}
+              value={countryState}
+            />
+          </div>
+          <div className="warehouse__form-bottom">
+            <h3 className="warehouse__form-subhead">Contact Details</h3>
+            <label className="warehouse__form-label" htmlFor="warehouseContact">
+              Contact Name
+            </label>
+            <input
+              type="text"
+              placeholder="Contact Name"
+              className="warehouse__input"
+              name="warehouseContact"
+              onChange={handleChangeContact}
+              value={contactState}
+            />
+            <label
+              className="warehouse__form-label"
+              htmlFor="warehousePosition"
+            >
+              Position
+            </label>
+            <input
+              type="text"
+              placeholder="Position"
+              className="warehouse__input"
+              name="warehousePosition"
+              onChange={handleChangePosition}
+              value={positionState}
+            />
+            <label className="warehouse__form-label" htmlFor="warehousePhone">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              placeholder="Phone"
+              className="warehouse__input"
+              name="warehousePhone"
+              onChange={handleChangePhone}
+              value={phoneState}
+            />
+            <label className="warehouse__form-label" htmlFor="warehouseEmail">
+              Email
+            </label>
+            <input
+              type="text"
+              placeholder="Email"
+              className="warehouse__input"
+              name="warehouseEmail"
+              onChange={handleChangeEmail}
+              value={emailState}
+            />
+          </div>
+        </div>
+        <div className="warehouse__buttons">
+          <button className="warehouse__button warehouse__button--secondary">
+            Cancel
+          </button>
+          <button className="warehouse__button warehouse__button--primary">
+            Add Warehouse
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PostWarehouse;
