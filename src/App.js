d011cccda5057e5e64de4efcb818fb7ccde9ac8e
Gurpreet Randhawa <singhpreet01991@gmail.com>
2022-11-22 17:48:07 -0800

    add delete warehouse modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 /* ------------------ SCSS IMPORTS ------------------ */
 import "./App.scss";
 /* ---------------- COMPONENT IMPORTS ---------------- */
-import Header from "./Components/Header/Header";
+import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
-import Footer from "./Components/Footer/Footer";
+import Footer from "./components/Footer/Footer";
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
+import WarehousedeleteModal from "./components/Modals/WarehouseDeleteModal";
+import InventoryDeleteModal from "./components/Modals/InventoryDeleteModal";
+
 /* ---------------- REACT IMPORTS ---------------- */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,10 +15,15 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
+      <WarehouseDetails />
+      <WarehousedeleteModal />
+
+      <Footer />
+
+      {/* <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path"/" element={<Footer />}
-      </Routes>
+        <Route path="/" element={<Footer />} />
+      </Routes> */}
     </BrowserRouter>
   );
 };
